3267d6aef7bd8d2f38c9142d5be29694cedc9d14
Nishant <Nishant>
2018-10-01 09:56:04 -0400

    First Commit

---
+++ src/main/java/com/petstore/auth/PetStoreAuthServerApplication.java
@@ -0,0 +1,12 @@
+package com.petstore.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetStoreAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetStoreAuthServerApplication.class, args);
+	}
+}
